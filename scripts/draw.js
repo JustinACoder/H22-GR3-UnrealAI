027a3fe9ddbb703c14741332f10c7914aac2c90a
JustAGuy <Justin.tovich@gmail.com>
2022-04-04 10:41:24 -0400

    Added quick draw example (to perfect)

---
+++ scripts/draw.js
@@ -0,0 +1,48 @@
+/*
+* In this file, we are going to draw the canvas.
+* If the user presses the mouse, we are going to draw a point at the mouse position of radius 5.
+* */
+
+let isDrawing = false;
+let prevX = null;
+let prevY = null;
+radius = 20;
+
+
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+
+window.addEventListener('mousedown', function (e) {
+    isDrawing = true;
+    prevX = e.clientX - canvas.offsetLeft;
+    prevY = e.clientY - canvas.offsetTop;
+    draw(e.clientX, e.clientY);
+});
+
+canvas.addEventListener('mousemove', function (e) {
+    if (isDrawing) {
+        draw(e.clientX, e.clientY);
+    }
+});
+
+window.addEventListener('mouseup', function () {
+    isDrawing = false;
+});
+
+function draw(x,y) {
+    //calculate position of the mouse relative to the canvas
+    let mouseX = x - canvas.offsetLeft;
+    let mouseY = y - canvas.offsetTop;
+
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(mouseX, mouseY);
+    ctx.lineWidth = radius * 2;
+    ctx.lineCap = 'round';
+    ctx.stroke();
+    ctx.closePath();
+
+    prevX = mouseX;
+    prevY = mouseY;
+}
+
